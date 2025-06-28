eaa17ffcbdf718a04ad5f62eee2604c87a89033c
NathanProfirio <gooyjoker1179@gmail.com>
2025-06-28 18:19:13 -0300

    atualização nos caminhos do site

--- validador/urls.py
+++ validador/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('consulta/', views.consulta_cpf, name="consulta_cpf")
+    path('', views.consulta_cpf, name="consulta_cpf")
 
 ]
--- validador_de_cpf/urls.py
+++ validador_de_cpf/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('validador_de_cpf/', include('validador.urls'))
+    path('', include('validador.urls'))
 ]
